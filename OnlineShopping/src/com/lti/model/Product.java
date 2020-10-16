7ebb67b9f4884ee73f6d7bf19d51ef067f8d741b
Muthualamelu <66001728+Muthualamelu@users.noreply.github.com>
2020-10-16 10:23:37 +0530

    Update Product.java

--- OnlineShopping/src/com/lti/model/Product.java
+++ OnlineShopping/src/com/lti/model/Product.java
@@ -5,6 +5,14 @@ public class Product {
 	int productId;
 	String productName;
 	String productDesc;
+	int quantity;
+	double price;
+	public double getPrice(){
+		return price;
+	}
+	public int getQuantity(){
+		return quantity;
+	}
 	
 	public int getProductId() {
 		return productId;
