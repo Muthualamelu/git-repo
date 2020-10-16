f0539f8dfb9dc606c3d59ca27622277db5e7b122
Muthu Alamelu <Muthu Alamelu@LAPTOP-VDCIEJQ2>
2020-10-16 09:51:08 +0530

    added account class

---
+++ OnlineShopping/src/com/lti/model/Account.java
@@ -0,0 +1,20 @@
+package com.lti.model;
+
+public class Account {
+
+	int AccountId;
+	int Balance;
+	public int getAccountId() {
+		return AccountId;
+	}
+	public void setAccountId(int accountId) {
+		AccountId = accountId;
+	}
+	public int getBalance() {
+		return Balance;
+	}
+	public void setBalance(int balance) {
+		Balance = balance;
+	}
+	
+}
